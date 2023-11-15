b821480c1990083a7332924f305bac59e25e25d8
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2023-11-15 17:08:17 +0900

    refactor: 내부함수 private 선언

--- src/Controller/Event.js
+++ src/Controller/Event.js
@@ -22,18 +22,18 @@ class Event{
     async runEvent(){
         OutputView.printWelcomeMsg()
         this.#date = await InputView.readDate()
-        this.#menu = await this.getMenuInfo()
+        this.#menu = await this.#getMenuInfo()
         OutputView.printPreviewMsg(this.#date)
-        this.printMenu()
-        this.printTotalPrice()
+        this.#printMenu()
+        this.#printTotalPrice()
         OutputView.printGift(this.#totalPrice)
-        this.printBenefit(this.#discount)
+        this.#printBenefit(this.#discount)
         OutputView.printTotalDiscount(this.#discount + this.#giftPrice)
         OutputView.printFinalPrice(this.#totalPrice, this.#discount)
         OutputView.printBadge(this.#discount + this.#giftPrice)
     }
 
-    printMenu(){
+    #printMenu(){
         OutputView.printMenuMsg()
         this.#menu.map(menu => {
             const name = menu.getName()
@@ -42,7 +42,7 @@ class Event{
         })
     }
 
-    printTotalPrice(){
+    #printTotalPrice(){
         this.#totalPrice = 0
         this.#menu.map(menu => {
             const count = menu.getCount()
@@ -52,20 +52,20 @@ class Event{
         OutputView.printTotalPrice(this.#totalPrice)
     }
 
-    printBenefit(){
+    #printBenefit(){
         OutputView.printBenefitMsg()
         this.#discount = 0
-        this.#discount += this.christmasDiscount()
-        if(this.isWeekday())
-            this.#discount += this.weekDayDiscount()
-        else this.#discount += this.weekendDiscount()
-        this.#discount += this.specialDiscount()
-        this.#giftPrice = this.giftDiscount()
+        this.#discount += this.#christmasDiscount()
+        if(this.#isWeekday())
+            this.#discount += this.#weekDayDiscount()
+        else this.#discount += this.#weekendDiscount()
+        this.#discount += this.#specialDiscount()
+        this.#giftPrice = this.#giftDiscount()
 
         if((this.#discount+this.#giftPrice) === 0) OutputView.printMsg(`없음`)
     }
 
-    christmasDiscount(){
+    #christmasDiscount(){
         let discount = 0
         if(this.#date <= 25) {
             discount = 1000
@@ -75,7 +75,7 @@ class Event{
         return discount
     }
 
-    weekDayDiscount(){
+    #weekDayDiscount(){
         let discount = 0
         this.#menu.map(menu => {
             if(menu.getType() === '디저트') discount += (2023 * menu.getCount())
@@ -84,7 +84,7 @@ class Event{
         return discount
     }
 
-    weekendDiscount(){
+    #weekendDiscount(){
         let discount = 0
         this.#menu.map(menu => {
             if(menu.getType() === '메인') discount += (2023 * menu.getCount())
@@ -93,12 +93,12 @@ class Event{
         return discount
     }
 
-    isWeekday(){
+    #isWeekday(){
         if(this.#date%7 !== 1 && this.#date%7 !== 2) return true
         return false
     }
 
-    specialDiscount(){
+    #specialDiscount(){
         if(this.#date%7 === 3 || this.#date === 25){
             OutputView.printMsg(`특별 할인: -1,000원`)
             return 1000
@@ -106,7 +106,7 @@ class Event{
         return 0
     }
 
-    giftDiscount(){
+    #giftDiscount(){
         if(this.#totalPrice > 120000){
             OutputView.printMsg(`증정 이벤트: -25,000원`)
             return 25000
@@ -114,11 +114,11 @@ class Event{
         return 0
     }
 
-    async getMenuInfo(){
+    async #getMenuInfo(){
         const menuInfo = await InputView.readMenuInfo()
 
-        this.checkMenuValidity(menuInfo) // 메뉴 형식이 예시와 다른지 체크 
-        const result = this.checkMenuDuplicate(menuInfo) // 중복 메뉴가 있는지 체크
+        this.#checkMenuValidity(menuInfo) // 메뉴 형식이 예시와 다른지 체크 
+        const result = this.#checkMenuDuplicate(menuInfo) // 중복 메뉴가 있는지 체크
 
         const menuList = []
         result.map(item => {
@@ -128,12 +128,12 @@ class Event{
         return menuList
     }
 
-    checkMenuValidity(menuInfo){
+    #checkMenuValidity(menuInfo){
         if(!menuInfo.includes('-')) throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.')
     }
 
-    checkMenuDuplicate(menuInfo){
-        const menuList = this.handleMenuInfo(menuInfo) // str to array
+    #checkMenuDuplicate(menuInfo){
+        const menuList = this.#handleMenuInfo(menuInfo) // str to array
         const names = menuList.map(item => item.name)
         const duplicateNames = names.filter((name, i) => names.indexOf(name) !== i)
 
@@ -143,7 +143,7 @@ class Event{
         return menuList
     }    
     
-    handleMenuInfo(menuInfo){
+    #handleMenuInfo(menuInfo){
         const result = []   
         menuInfo.split(',').forEach(pair => {
             const [menuName, count] = pair.split('-')
